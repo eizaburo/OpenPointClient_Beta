9cdebb70614a94cc3ce46dd60575c6728222e327
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-12-03 17:53:04 +0900

    サインアップ画面にFormik導入

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -5,36 +5,101 @@ import { Card, FormLabel, FormInput, FormValidationMessage, Button, CheckBox } f
 //Auth
 import { onSignIn } from '../Auth';
 
+//formik
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+
 class SignUp extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, paddingVertical: 20 }}>
-                <Card title='サインアップ'>
-                    <FormLabel>Name</FormLabel>
-                    <FormInput />
-                    <FormValidationMessage>error</FormValidationMessage>
-                    <FormLabel>Email</FormLabel>
-                    <FormInput />
-                    <FormValidationMessage>error</FormValidationMessage>
-                    <FormLabel>Password</FormLabel>
-                    <FormInput />
-                    <FormValidationMessage>error</FormValidationMessage>
-                    <FormLabel>Password（確認）</FormLabel>
-                    <FormInput />
-                    <FormValidationMessage>error</FormValidationMessage>
-                    <View style={{ paddingTop: 10 }}>
-                        <CheckBox
-                            center
-                            title='同意する'
-                        />
-                    </View>
-                    <Button
-                        title='サインアップ'
-                        onPress={() => this.handleSignUp()}
-                        buttonStyle={{ marginTop: 20 }}
-                        backgroundColor='#CC9933'
-                    />
-                </Card>
+                <Formik
+                    initialValues={{
+                        name: '',
+                        email: '',
+                        password: '',
+                        passwordConfirm: '',
+                        check: false,
+                    }}
+                    onSubmit={() => this.handleSignUp()}
+                    validationSchema={Yup.object().shape({
+                        name: Yup
+                            .string().matches(/^(^[a-zA-Z0-9\-_\.]+$)/, '半角英数字のみで入力してください。')
+                            .min(4, '名前は4文字以上8文字以内で入力して下さい。')
+                            .max(16, '名前は4文字以上16文字以内で入力して下さい。')
+                            .required('名前の入力は必須です。'),
+                        email: Yup
+                            .string()
+                            .email('Emailの形式ではないようです。')
+                            .required('Emailの入力は必須です。'),
+                        password: Yup
+                            .string()
+                            .min(4, '4文字以上で入力して下さい。')
+                            .required('パスワードの入力は必須です。'),
+                        passwordConfirm: Yup
+                            .string()
+                            .required('パスワードの確認は必須です。')
+                            .oneOf([Yup.ref('password')], 'パスワードが一致しません。'),
+                        check: Yup
+                            .boolean().oneOf([true], '同意にチェックお願いします。')
+                    })}
+                >
+                    {
+                        ({ handleSubmit, handleChange, values, errors, handleBlur, touched, setFieldValue }) => (
+                            <Card title='サインアップ'>
+                                <FormLabel>Name</FormLabel>
+                                <FormInput
+                                    autoCapitalize='none'
+                                    value={values.name}
+                                    onChangeText={handleChange('name')}
+                                    onBlur={handleBlur('name')}
+                                />
+                                {(touched.name) && <FormValidationMessage>{errors.name}</FormValidationMessage>}
+                                <FormLabel>Email</FormLabel>
+                                <FormInput
+                                    autoCapitalize='none'
+                                    value={values.email}
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                />
+                                {(touched.email) && <FormValidationMessage>{errors.email}</FormValidationMessage>}
+                                <FormLabel>Password</FormLabel>
+                                <FormInput
+                                    autoCapitalize='none'
+                                    value={values.password}
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    secureTextEntry
+                                />
+                                {(touched.password) && <FormValidationMessage>{errors.password}</FormValidationMessage>}
+                                <FormLabel>Password（確認）</FormLabel>
+                                <FormInput
+                                    autoCapitalize='none'
+                                    value={values.passwordConfirm}
+                                    onChangeText={handleChange('passwordConfirm')}
+                                    onBlur={handleBlur('passwordConfirm')}
+                                    secureTextEntry
+                                />
+                                {(touched.passwordConfirm) && <FormValidationMessage>{errors.passwordConfirm}</FormValidationMessage>}
+                                <View style={{ paddingTop: 10 }}>
+                                    <CheckBox
+                                        center
+                                        title='同意する'
+                                        checked={values.check}
+                                        onPress={() => setFieldValue('check', !values.check)}
+                                    />
+                                    {(touched.check) && <FormValidationMessage>{errors.check}</FormValidationMessage>}
+                                </View>
+                                <Button
+                                    title='サインアップ'
+                                    onPress={handleSubmit}
+                                    buttonStyle={{ marginTop: 20 }}
+                                    backgroundColor='#CC9933'
+                                />
+                            </Card>
+                        )
+                    }
+                </Formik>
             </View>
         );
     }
