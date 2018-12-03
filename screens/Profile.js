90ea9a995ec5751b6ae2207088f3cc2be811f5f0
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-12-03 10:38:37 +0900

    プロファイルページレイアウト

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Card, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 
 class Profile extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Profile</Text>
+            <View style={{ flex: 1, paddingVertical: 20 }}>
+                <Card title='サインアウト'>
+                    <Button
+                        title='サインアウト'
+                        onPress={()=>this.handleSignOut()}
+                    />
+                </Card>
             </View>
         );
+    }
+
+    //サインアプトボタンクリック時
+    handleSignOut = () => {
+        this.props.navigation.navigate('SignedOut');
     }
 }
 
