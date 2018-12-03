21c81159ee609cef2abb403db7054e7c9a7289c5
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-12-03 10:48:33 +0900

    サインアップ画面レイアウト

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Card, FormLabel, FormInput, FormValidationMessage, Button, CheckBox } from 'react-native-elements';
 
 class SignUp extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>SignUp</Text>
+            <View style={{ flex: 1, paddingVertical: 20 }}>
+                <Card title='サインアップ'>
+                    <FormLabel>Name</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>error</FormValidationMessage>
+                    <FormLabel>Email</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>error</FormValidationMessage>
+                    <FormLabel>Password</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>error</FormValidationMessage>
+                    <FormLabel>Password（確認）</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>error</FormValidationMessage>
+                    <View style={{ paddingTop: 10 }}>
+                        <CheckBox
+                            center
+                            title='同意する'
+                        />
+                    </View>
+                    <Button
+                        title='サインアップ'
+                        onPress={() => this.handleSignUp()}
+                        buttonStyle={{ marginTop: 20 }}
+                        backgroundColor='#CC9933'
+                    />
+                </Card>
             </View>
         );
+    }
+
+    //サインアウトボタンクリック時
+    handleSignUp = () => {
+        this.props.navigation.navigate('SignedIn');
     }
 }
 
