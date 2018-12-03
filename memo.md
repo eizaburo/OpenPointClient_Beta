5b01f8c72572c10b9b8c0be2c4f839771c138108
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-12-03 10:35:13 +0900

    サインインページレイアウト

--- memo.md
+++ memo.md
@@ -2,3 +2,7 @@
 
 mkdir screens
 ouch screens/Home.js screens/Profile.js screens/SignIn.js screens/SignUp.js screens/Drawer.js
+
+yarn add react-navigation
+
+yarn add react-native-navigation
--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Card, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 
 class SignIn extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>SignIn</Text>
+            <View style={{ flex: 1, paddingVertical: 20 }}>
+                <Card title='サインイン'>
+                    <FormLabel>Email</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>error</FormValidationMessage>
+                    <FormLabel>Password</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>error</FormValidationMessage>
+                    <Button
+                        title='サインイン'
+                        onPress={() => this.handleSignIn()}
+                        buttonStyle={{ marginTop: 20 }}
+                        backgroundColor='#0099FF'
+                    />
+                </Card>
+                <Card title='サインアップ'>
+                    <Button
+                        title='サインアップ'
+                        onPress={() => this.handleSignUp()}
+                        buttonStyle={{ marginTop: 0 }}
+                        backgroundColor='#CC9933'
+                    />
+                </Card>
             </View>
         );
+    }
+
+    //サインインボタンクリック時
+    handleSignIn = () => {
+        this.props.navigation.navigate('SignedIn');
+    }
+
+    //サインアウトボタンクリック時
+    handleSignUp = () => {
+        this.props.navigation.navigate('SignUp');
     }
 }
 
