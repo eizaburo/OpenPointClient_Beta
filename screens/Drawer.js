df9b80003925f6885ee6cd4249be98cf6ba8f52d
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-12-03 10:40:07 +0900

    Drawerページレイアウト（Profileそのまま流用）

--- screens/Drawer.js
+++ screens/Drawer.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Card, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 
 class Drawer extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Drawer</Text>
+            <View style={{ flex: 1, paddingVertical: 20 }}>
+                <Card title='サインアウト'>
+                    <Button
+                        title='サインアウト'
+                        onPress={()=>this.handleSignOut()}
+                    />
+                </Card>
             </View>
         );
+    }
+
+    //サインアプトボタンクリック時
+    handleSignOut = () => {
+        this.props.navigation.navigate('SignedOut');
     }
 }
 
