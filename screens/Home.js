1312d304644cc3838ea27734566c3b68bbd552ba
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-12-04 07:49:31 +0900

    Homeをテンプレートとして利用するためimport等追加

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Card, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
+
+//Auth
+import { isSignedIn } from '../Auth';
+
+//redux
+import { connect } from 'react-redux';
+import { updateUserData } from '../actions/userAction';
+
+//config
+import * as appConfig from '../app.config';
+
+//devlib
+import * as devlib from '../DevLib';
 
 class Home extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home</Text>
+            <View style={{ flex: 1, paddingVertical: 20 }}>
+                <Card>
+                    <Text>Home</Text>
+                </Card>
             </View>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = state => (
+    {
+        state: state,
+    }
+);
+const mapDispatchToState = dispatch => (
+    {
+        updateUserData: user => dispatch(updateUserData(user)),
+    }
+);
+export default connect(mapStateToProps, mapDispatchToState)(Home);
+// export default Home;
