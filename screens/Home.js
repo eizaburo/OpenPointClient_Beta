9435761c9cf11e0d1bc3bf9edc876436cac2563e
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-12-07 06:54:51 +0900

    Homeにおすすめ情報表示

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { Card, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 
 //Auth
@@ -20,12 +20,35 @@ import * as appConfig from '../app.config';
 import * as devlib from '../DevLib';
 
 class Home extends React.Component {
+
+    state = {
+        recommends: devlib.recommends,
+    }
+
     render() {
         return (
             <View style={{ flex: 1, paddingVertical: 20 }}>
-                <Card>
-                    <Text>Home</Text>
+                <Card title='おすすめ情報'>
+                    <Text>{this.props.state.userData.user.name}さんとしてサインインしています。</Text>
                 </Card>
+                <FlatList
+                    data={this.state.recommends}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={({ item }) => (
+                        <Card
+                            title={item.title}
+                            image={{ uri: item.image }}
+                        >
+                            <Button
+                                title="もっと見る"
+                                backgroundColor='#CC9933'
+                                borderRadius={20}
+                                icon={{ name: 'eye', type: 'octicon' }}
+                                onPress={() => alert(item.id)}
+                            />
+                        </Card>
+                    )}
+                />
             </View>
         );
     }
